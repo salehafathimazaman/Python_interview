1137e52d547f5671106b46bb99d664ce078c9527
salehafathimazaman <your_email@example.com>
2025-11-12 12:08:23 -0500

    added even and odd

--- thirs_day/number_counter.py
+++ thirs_day/number_counter.py
@@ -1,4 +1,28 @@
 user_input=input("enter the number till which the counting should go on:")
 number=int(user_input)
-for i in range(1, number+1):
-    print(i)
+
+def allnumbers(number):
+    for i in range(1, number+1):
+        print(i)
+
+def evennumbers(number):
+    for i in range(1, number+1):
+        if i%2==0:
+            print(i)
+
+def oddnumbers(number):
+    for i in range(1, number+1):
+        if i%2!=0:
+            print(i)
+
+option_input=input("select a for all numbers, b for even only and c for oddonly: ")
+
+if option_input=='a':
+    allnumbers(number)
+elif option_input=='b':
+    evennumbers(number)
+elif option_input=='c':
+    oddnumbers(number)
+
+
+
