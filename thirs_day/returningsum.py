deee7231c4a73135b4e431e45bd163abe2c41375
salehafathimazaman <sfzhsn@gmail.com>
2025-11-13 12:11:54 -0500

    hopefully this gets recorded

---
+++ thirs_day/returningsum.py
@@ -0,0 +1,11 @@
+number_1=input("give a number A= :")
+number_2=input("give a number B= :")
+A=int(number_1)
+B=int(number_2)
+def returnvalue(A, B):
+    return A+B
+
+result=returnvalue(A, B)
+print(f"sum of {A} and {B} is {result}")
+      
+
