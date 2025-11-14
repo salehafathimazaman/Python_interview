6bdcb1380829dfcc76f551e063001516568f1a91
salehafathimazaman <sfzhsn@gmail.com>
2025-11-14 11:49:01 -0500

    fifth day..hoping this shows up

---
+++ fifth_day/multple return values.py
@@ -0,0 +1,12 @@
+user_input=input("enter A:")
+user_input_2=input("enter B:")
+A=int(user_input)
+B=int(user_input_2)
+def multiplereturns(A, B):
+    return A+B,A-B,A*B
+
+sumvalue, diff_value, product_value= multiplereturns(A, B)
+
+
+print(f"sum of A and B = {sumvalue} differenc eof A and b is {diff_value} product of A and B is {product_value}")
+
